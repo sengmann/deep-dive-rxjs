6d1f947ee6698a7f6902104fc8f46d74d07dc32c
Sascha Engmann <engmann@w11k.de>
2020-01-24 09:32:07 +0100

    write intro, add file for operators

--- src/01_intro.ts
+++ src/01_intro.ts
@@ -41,6 +41,10 @@
 // Q: What do you need it for?
 // A: RxJS is a library for composing asynchronous and event-based programs by using observable sequences.
 
+// Q: what are the most important properties of observables?
+// A: Observables are lazy and can deliver more than one value over time
+
+
 
 
 
@@ -50,9 +54,58 @@
 
 
 // Hello from the observable world...
-import { of } from 'rxjs';
+import { empty, EMPTY, from, fromEvent, interval, of, timer } from "rxjs";
+import { take } from 'rxjs/operators';
+import { fromPromise } from 'rxjs/internal-compatibility';
+
+const EventEmitter = require("events").EventEmitter;
+const clickEventEmitter = new EventEmitter();
 
 of(1, 2, 3).subscribe(n => console.log(n));
+
+
+
+
+// Creating Observables
+
+from([1, 2, 3]).subscribe(_ => console.log(_));
+
+timer(100).subscribe(_ => console.log(_));
+
+empty();
+
+
+// But why all of them have to end?
+
+fromEvent(clickEventEmitter, "buttonClick")
+    .pipe(take(1))
+    .subscribe(_ => console.log(_));
+clickEventEmitter.emit("buttonClick", { type: "MouseEvent", x: 100, y: 100 });
+
+interval(1000)
+    .pipe(take(5))
+    .subscribe(_ => console.log(_));
+
+
+// And what about Promises?
+
+fromPromise(Promise.resolve('abc')).subscribe(_ => console.log(_));
+of(1).toPromise().then(_ => console.log(_));
+
+
+
+
+// How to describe when and what is happening?
+
+// We are using so called marble diagrams to describe the time sequence.
+
+/**
+ * empty() => '|'
+ * of(1) => '1|'
+ * timer(10, 3) => '-0-1-2|'
+ */
+
+
 
 
 
