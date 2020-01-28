9ced8dbbbe9ea4fa9522b368fd27cf76cc1009cf
Sascha Engmann <engmann@w11k.de>
2020-01-28 11:00:01 +0100

    operator examples completed

--- src/02_operators.ts
+++ src/02_operators.ts
@@ -16,7 +16,7 @@
 
 // Starting simple
 
-import { concat, empty, of, throwError } from "rxjs";
+import { combineLatest, concat, empty, interval, Observable, of, throwError, zip } from "rxjs";
 import {
     catchError,
     distinctUntilChanged,
@@ -29,6 +29,7 @@ import {
     take,
     tap
 } from "rxjs/operators";
+import { combineLatestToMap } from "@w11k/rx-ninja";
 
 of("1", "2", "3")
     .pipe(
@@ -123,9 +124,33 @@ throwError("bad things happen")
 
 
 
+
+
+
+
+
+
+
 // combining
 concat(of(1, 2, 3), of(4, 5, 6))
     .subscribe(_ => console.log(_));
+
+
+
+
+
+
+
+const a = interval(1000).pipe(take(3));
+const b = interval(2000).pipe(take(2));
+
+zip(a, b)
+    .subscribe(_ => console.log(_));
+
+combineLatest([a, b])
+    .subscribe(_ => console.log(_));
+
+
 
 
 /* https://rxviz.com/examples/custom
@@ -138,11 +163,72 @@ merge(
 */
 
 
+/* https://rxviz.com/examples/custom
+const { fromEvent, of, interval } = Rx;
+const { mergeMap, map, switchMap, delay } = RxOperators;
+
+// faking network request for save
+const save = (x) => {
+  return of(x).pipe(delay(500))
+  //return interval(500).pipe(map(() => x));
+};
+
+const click$ = fromEvent(document, 'click');
+
+click$
+  .pipe( mergeMap(e => save(e.clientX)) )
+*/
 
 
 
 
 
+
+
+
+// Can't find the operator to solve your problem? Don't mind and write your own
+// A pipeable operator is a function that is passed an observable and must return a result observable.
+
+const canContainNull: Observable<string | null> = of("a", "ab", "ccc", null, "d");
+
+function isNotNull<T>(t: T | null): t is T {
+    return t !== null;
+}
+
+
+const lengthOfStrings = canContainNull.pipe(
+    filter(isNotNull), // works but not exactly easy to use
+    map(x => x.length)
+);
+
+lengthOfStrings.subscribe(_ => console.log(_));
+
+function skipNull<T>() {
+    return (input: Observable<T | null>) => input.pipe(filter(isNotNull));
+}
+
+canContainNull.pipe(skipNull()).subscribe(_ => console.log(_));
+
+
+
+
+
+const foo$ = of(1, 2, 3, 4);
+const bar$ = of("5", "3", "4");
+
+// What happens when we switch foo$ and bar$?
+combineLatest([foo$, bar$])
+    .pipe(
+        map(([f, b]) => f + parseInt(b, 10)),
+    ).subscribe(_ => console.log(_));
+
+
+
+// Looks very similar, but we can access the values using the property name instead of the position in the array
+combineLatestToMap({ f: foo$, b: bar$ })
+    .pipe(
+        map(({ f, b }) => f + parseInt(b, 10)),
+    ).subscribe(_ => console.log(_));
 
 
 
